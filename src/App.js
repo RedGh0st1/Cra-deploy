180748f77ab937719776457a62d4de28810a027b
Lennie Nurse <lnursej@gmail.com>
2022-10-14 11:45:51 -0400

    added h2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css"
 
 function App() {
+  console.log(process.env.REACT_APP_API_KEY)
   return (
     <div className="App">
       <h1>Pursuit App Template</h1>
+      <h2>My New Update</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
